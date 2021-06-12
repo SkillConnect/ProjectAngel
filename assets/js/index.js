aade29882bfe4e375af6390c8890c37f6adb5dee
Adish Pathare <adish.pathare@gmail.com>
2021-06-12 22:34:16 +0530

    table on dashboard

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+displayClientDetails();
+
+async function displayClientDetails() {
+	var snapshot = await firebase.database().ref(`clients`).once('value');
+	var data = snapshot.val();
+	eachRow = ""
+	jQuery.each(data, function(ID, details) {
+		// console.log(ID, details)
+        if(details) {
+        	let marketCategory = details.marketCategory;
+        	let multiplier = details.multiplier;
+        	let name = details.name;
+        	let password = details.password;
+        	let status = details.status;
+        	let userID = details.userID;
+
+        	eachRowContent = `<tr> <td>${ID}</td>
+        	<td>${name}</td> 
+        	<td>${userID}</td> 
+        	<td>${password}</td>
+        	<td>${marketCategory}</td>
+        	<td>${multiplier}</td>
+        	<td>${status}</td>
+        	<td><div class="d-flex">
+                    <a class="btn btn-warning" href="">Edit</a> </div> </td> 
+            <td><div class="d-flex">
+                    <a class="btn btn-primary" href="">Delete</a> </div> </td> </tr>`
+        	eachRow += eachRowContent
+        }
+    })
+    document.getElementById("clientDetails").innerHTML = eachRow;
+}
