9e92c0d228e41e4594f274bad99d8a43ee12fe5a
Mukund Choudhary <mukundchoudhary126@gmail.com>
2021-06-20 20:29:57 +0530

    Optimising branch

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,26 +1,49 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyDrNCCRVkU9g5ZR10qPNGqPHoC0K9evjCc',
+  authDomain: 'projectangel-1cd7f.firebaseapp.com',
+  projectId: 'projectangel-1cd7f',
+  databaseURL: 'https://projectangel-1cd7f-default-rtdb.firebaseio.com/',
+  storageBucket: 'projectangel-1cd7f.appspot.com',
+  messagingSenderId: '677738651351',
+  appId: '1:677738651351:web:f4fe164e48d110940dda92'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  const authPages = ['login']
+  var currentPage = window.location.pathname.split('/')
+  var currentPage = currentPage[currentPage.length-1].split('.')[0]
+
+  if (user && authPages.includes(currentPage)) {
+      window.location.href = 'index.html'
+  } 
+  else if(!user && !authPages.includes(currentPage)) {
+    // User is NOT signed in, redirecting to auth page
+    window.location.href = 'login.html'
+  }
+});
+
 function login(){
-    var email = document.getElementById('email').value
-    var password = document.getElementById('password').value
-    console.log(email,password)
+    document.getElementById('submitBtn').innerHTML = 'Logging In...'
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     firebase.auth().signInWithEmailAndPassword(email, password)
-     .then((userCredential) => {
-    // Signed in
-    window.location.href="index.html"
-    console.log('Log in')
-    })
-   .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage,errorCode)
-   });
+      .then((userCredential) => {
+        window.location.href = 'index.html';
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(`${errorMessage} ${errorCode}`);
+        window.location.reload()
+      });
+    return false
 }
 
 function signout(){
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        window.location.href="login.html"
-        console.log('BYE')
-      }).catch((error) => {
-        // An error happened.
-      });
+  firebase.auth().signOut().then(() => {
+    window.location.href = 'login.html'
+  })
 }
--- assets/js/profile.js
+++ assets/js/profile.js
@@ -5,21 +5,6 @@ firebase.auth().onAuthStateChanged((user) => {
     }
 })
 
-// console.log("Hello")
-firebase.auth().onAuthStateChanged((user) => {
-    const authPages = ['login']
-    var currentPage = window.location.pathname.split('/')
-    var currentPage = currentPage[currentPage.length-1].split('.')[0]
-
-    if (user && authPages.includes(currentPage)) {
-        window.location.href = 'index.html'
-    } 
-    else if(!user && !authPages.includes(currentPage)) {
-      // User is NOT signed in, redirecting to auth page
-      window.location.href = 'login.html'
-    }
-});  
-
 async function angelBrokingProfileDetails() {
 	var snapshot = await firebase.database().ref(`owner`).once('value');
 	var data = snapshot.val();
@@ -36,56 +21,51 @@ async function firebaseProfileDetails() {
 }
 
 function updateAngelBrokingDetails() {
+	document.getElementById('abBtn').innerHTML = 'Updating...'
 	var abUsername = document.getElementById('abUsername').value;
-	if (abUsername == "") { confirm("Username Can't be Empty"); exit();}
+	if (abUsername == "") { alert("Username Can't be Empty"); return}
 	var abPassword = document.getElementById('abPassword').value;
 	var abPassword2 = document.getElementById('abPassword2').value;
-	// console.log(abUsername, abPassword)
 	if((abPassword == abPassword2) && (abPassword != "" )) {
-		if(confirm("Confirm To Change Angel Broking Details") == true) {
+		if(confirm("Confirm To Change Angel Broking Details")) {
 			firebase.database().ref('owner').update({AngelBrokingID: abUsername})
-			firebase.database().ref('owner').update({AngelBrokingPassword: abPassword}); 
+			firebase.database().ref('owner').update({AngelBrokingPassword: abPassword});
+			alert('Details updated successfully!')
 		}
 	}
 	else {
 		alert("Password Doesn't Match! OR Password Can't be Empty");
-	}	
+	}
 }
 
 function updateFirebaseDetails() {
-	// var user = firebase.auth().currentUser;
+	document.getElementById('fbBtn').innerHTML = 'Updating...'
 	var fbEmail = document.getElementById('fbEmail').value;
-	if (fbEmail == "") { confirm("Email Can't be Empty"); exit();}
-
+	if (fbEmail == "") { alert("Email Can't be Empty"); return}
 	var fbCurrentPassword = document.getElementById('fbCurrentPassword').value;
-	console.log("fbCurrentPassword", fbCurrentPassword)
 	var fbPassword = document.getElementById('fbPassword').value;
 	var fbPassword2 = document.getElementById('fbPassword2').value;
-	console.log(fbPassword, fbPassword2)
 	if((fbPassword == fbPassword2) && (fbPassword != "") ) {
-		// First Login then Update the Password
 		firebase.auth().signInWithEmailAndPassword(fbEmail, fbCurrentPassword)
-     	.then((userCredential) => {
-     		var user = userCredential.user;
-    		// after Succesfull Login - Update Password
-    		if(confirm("Confirm to change Firebase Credentials ?")) {
-				user.updatePassword(fbPassword).then(() => {
-				// Update successful.
-				}).catch((error) => {
-					console.log(error);
-				});
-				console.log('Log in')
-			}
-			else {
-				alert("No Update in Firebase Credentials")
-			}
-    	}).catch((error) => {
-  			// An error ocurred
-  			console.log("Failed Login Internally",error)
-		});
+			.then((userCredential) => {
+				var user = userCredential.user;
+				// after Succesfull Login - Update Password
+				if(confirm("Confirm to change Firebase Credentials ?")) {
+					user.updatePassword(fbPassword)
+						.then(() => {
+							alert('Successfully updated password')
+						}).catch((error) => {
+							alert(error);
+						});
+				}
+				else {
+					alert("No Update in Firebase Credentials")
+				}
+			}).catch((error) => {
+				alert(error)
+			});
 	}
 	else {
 		alert("Password Doesn't Match! OR Password Can't be Empty");
 	}
-
 }
